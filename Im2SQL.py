7cd8387ea0e7711e8f6637fcf94aca663d21886b
anuran <anuranroy02@gmail.com>
2021-10-19 01:28:15 +0530

    Fixed parsing errors

--- Im2SQL.py
+++ Im2SQL.py
@@ -6,10 +6,11 @@ import cv2
 import pytesseract
 import numpy as np
 
+
 def recognize(img_path, tesseract_path):  # Taken from GFG
 
     # Mention the installed location of Tesseract-OCR in your system
-    pytesseract.pytesseract.tesseract_cmd = tesseract_path  # '/usr/bin/tesseract'
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
     # Read image from which text needs to be extracted
     img = cv2.imread(img_path)
@@ -30,19 +31,20 @@ def recognize(img_path, tesseract_path):  # Taken from GFG
     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
     # Applying dilation on the threshold image
-    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
 
     # Finding contours
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-                                                    cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(
+        dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+    )
 
     # Creating a copy of image
     im2 = img.copy()
 
     # A text file is created and flushed
-#     file = open(output_path, "w+")
-#     file.write("")
-#     file.close()
+    #     file = open(output_path, "w+")
+    #     file.write("")
+    #     file.close()
 
     # Looping through the identified contours
     # Then rectangular part is cropped and passed on
@@ -52,30 +54,31 @@ def recognize(img_path, tesseract_path):  # Taken from GFG
         x, y, w, h = cv2.boundingRect(cnt)
 
         # Drawing a rectangle on copied image
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # Cropping the text block for giving input to OCR
-        cropped = im2[y:y + h, x:x + w]
+        cropped = im2[y : y + h, x : x + w]
 
         # Open the file in append mode
-#         file = open(output_path, "a")
+        #         file = open(output_path, "a")
 
         # Apply OCR on the cropped image
         text = pytesseract.image_to_string(cropped)
 
         # Appending the text into file
-#         file.write(text)
-#         file.write("\n")
+    #         file.write(text)
+    #         file.write("\n")
 
-        # Close the file
-#         file.close
-    
+    # Close the file
+    #         file.close
+
     return text
 
 
-text = recognize('employee.png', '/usr/bin/tesseract')
+text = recognize("tests/tesseract/input/employee.png", "/usr/bin/tesseract")
 
-print(text.strip().replace("|", ", ").replace("[",", "))
+# print(text.strip().replace("|", ", ").replace("[", ", "))
+
 
 def typecast(text, decimals=1):
     if text.isnumeric():
@@ -84,29 +87,29 @@ def typecast(text, decimals=1):
                 return int(text)
             else:
                 return np.round(float(text), decimals)
-        except:
+        finally:
             pass
     elif text.lower() == "null":
         return text
     else:
-        return f'{text}'
+        return f"'{text}'"
 
 
+tx2 = str(text.strip().replace("|", "; ").replace("[", "; "))
 
-tx2 = str(text.strip().replace("|", ", ").replace("[",", "))
+lines = [[typecast(j.strip(" "), 0) for j in i.split(";")] for i in tx2.split("\n")]
 
-lines = [[typecast(j.strip(" "), 0) for j in i.split(",")] for i in tx2.split("\n")]
-
-for i in lines:
-    print(i, end=",\n")
-
+# for i in lines:
+#     print(i, end=",\n")
 
 
 class TableNameException(Exception):
     def __init__(self):
-        self.msg = "\nYour table name isn't in one word. Consider using underscores (_) instead of spaces and hyphens(-)\n"
+        self.msg = """\nYour table name isn't in one word.
+                    Consider using underscores (_) instead of spaces
+                    and hyphens(-)\n
+                    """
         super().__init__(self.msg)
-
 
 
 table_name = input("Enter table name:")
@@ -114,12 +117,16 @@ table_name = input("Enter table name:")
 if len(table_name.strip().split(" ")) != 1 or table_name == "":
     raise TableNameException
 
+commands = list()
 for i in lines:
     cmd = f"INSERT INTO {table_name} VALUES("
     for j in range(len(i)):
         cmd += f"{i[j]}"
-        if j < len(i)-1:
+        if j < len(i) - 1:
             cmd += ", "
     cmd += ");"
-    print(cmd)
+    commands.append(cmd)
+    # print(cmd)
 
+for i in commands:
+    print(i)
